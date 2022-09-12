98c9bb013e4f77e13f66ab1039334fbc740058db
Mike Hukiewitz <mike.hukiewitz@robotter.ai>
2022-09-12 14:59:19 +0200

    add test_amending_record and test_store_and_index_library

--- test/aars.py
+++ test/aars.py
@@ -1,3 +1,5 @@
+from typing import List
+
 from src.aars import Record, Index
 import pytest
 
@@ -7,13 +9,16 @@ class Book(Record):
     author: str
 
 
+class Library(Record):
+    name: str
+    books: List[Book]
+
+
 @pytest.mark.asyncio
 async def test_store_and_index():
     Index(Book, 'title')
     new_book = await Book.create(title='Atlas Shrugged', author='Ayn Rand')
-    print(new_book)
     fetched_book = (await Book.query('Atlas Shrugged', 'Book.title'))[0]
-    print(fetched_book)
     assert new_book == fetched_book
 
 
@@ -21,3 +26,26 @@ async def test_store_and_index():
 async def test_fetch_all():
     books = await Book.fetch_all()
     assert len(books) > 0
+
+
+@pytest.mark.asyncio
+async def test_amending_record():
+    book = await Book.create(title='Neurodancer', author='William Gibson')
+    book.title = 'Neuromancer'
+    book = await book.upsert()
+    assert book.title == 'Neuromancer'
+    assert len(book.revision_hashes) == 2
+    assert book.current_revision == 1
+    assert book.revision_hashes[0] == book.item_hash
+    assert book.revision_hashes[1] != book.item_hash
+
+
+@pytest.mark.asyncio
+async def test_store_and_index_record_of_records():
+    Index(Library, 'name')
+    new_library = await Library.create(name='The Library', books=[
+        await Book.create(title='Atlas Shrugged', author='Ayn Rand'),
+        await Book.create(title='The Martian', author='Andy Weir')
+    ])
+    fetched_library = (await Library.query('The Library', 'Library.name'))[0]
+    assert new_library == fetched_library
